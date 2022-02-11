a45369d6a8579cf9a5d80bcb5f721805d09300bb
Miguel <miguelgas1234@gmail.com>
2022-02-11 17:14:23 +0100

    Creacion del metodo 'Perimetro_rectangulo'

--- src/com/company/Rectangulo.java
+++ src/com/company/Rectangulo.java
@@ -7,7 +7,11 @@ public class Rectangulo {
     public double Area_rectangulo(){
         return altura * ancho;
     }
-    
+
+    public double Perimetro_rectangulo(){
+        return (ancho*2)+(altura*2);
+    }
+
     public Rectangulo(double altura, double ancho){
         this.altura = altura;
         this.ancho = ancho;
